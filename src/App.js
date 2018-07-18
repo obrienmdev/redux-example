fe9015854baf586f84f98b4292615ad762a3f68d
Mark O'Brien <mark.obrien@autodesk.com>
2018-07-18 10:32:50 +0100

    Simplify App

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import React, { Component } from 'react'
+import React from 'react'
 import ItemsList from './ItemsList'
 import ItemInput from './ItemInput'
 import './App.css'
 
-class App extends Component {
-  render () {
-    return (
-      <div>
-        <ItemInput />
-        <ItemsList />
-      </div>
-    )
-  }
-}
+const App = () => (
+  <div>
+    <ItemInput />
+    <ItemsList />
+  </div>
+)
 
 export default App
